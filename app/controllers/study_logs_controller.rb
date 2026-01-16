486b4877223a78f52b328237b445e1eab7a03134
KURO-GH <k.s06020608@gmail.com>
2026-01-16 13:40:52 +0900

    学習記録投稿機能を追加

---
+++ app/controllers/study_logs_controller.rb
@@ -0,0 +1,27 @@
+class StudyLogsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @study_logs = current_user.study_logs
+  end
+
+  def new
+    @study_log = StudyLog.new
+  end
+
+  def create
+    @study_log = current_user.study_logs.build(study_log_params)
+
+    if @study_log.save
+      redirect_to study_logs_path, notice: '学習記録を投稿しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def study_log_params
+    params.require(:study_log).permit(:title, :content)
+  end
+end
