8d459800d5d0c10f698445d5fb844edd7c3cc511
KURO-GH <k.s06020608@gmail.com>
2026-01-16 13:40:52 +0900

    学習記録投稿機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-
-  root 'home#index'
+devise_for :users
+root 'home#index'
+resources :study_logs, only: [:index, :new, :create]
 end
---
+++ db/migrate/20260116040730_create_study_logs.rb
@@ -0,0 +1,11 @@
+class CreateStudyLogs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :study_logs do |t|
+      t.string :title
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
