5559d85ebb0194299255ff97466d2f0bb3b06140
KURO-GH <k.s06020608@gmail.com>
2026-01-19 14:59:39 +0900

    Add validations and Japanese i18n

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +14,8 @@ module MyGraduationApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.i18n.default_locale = :ja
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
