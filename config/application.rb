5641759e94bda2ffca97a1a8d4b308ced923ba0a
KURO-GH <k.s06020608@gmail.com>
2026-01-19 16:18:14 +0900

    UIの調整

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module MyGraduationApp
 
     config.i18n.default_locale = :ja
 
+    
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     # Configuration for the application, engines, and railties goes here.
