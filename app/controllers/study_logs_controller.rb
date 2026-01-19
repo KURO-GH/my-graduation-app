c60ccec0600f8354de98f709d2fe2608506955fa
KURO-GH <k.s06020608@gmail.com>
2026-01-19 14:10:43 +0900

    Add edit and delete feature for study logs

--- app/controllers/study_logs_controller.rb
+++ app/controllers/study_logs_controller.rb
@@ -1,5 +1,6 @@
 class StudyLogsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_study_log, only: [:edit, :update, :destroy]
 
   def index
     @study_logs = current_user.study_logs.order(created_at: :desc)
@@ -19,7 +20,27 @@ class StudyLogsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @study_log.update(study_log_params)
+      redirect_to study_logs_path, notice: '学習記録を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @study_log.destroy
+    redirect_to study_logs_path, notice: '学習記録を削除しました'
+  end
+
   private
+
+  def set_study_log
+    @study_log = current_user.study_logs.find(params[:id])
+  end
 
   def study_log_params
     params.require(:study_log).permit(:title, :content)
