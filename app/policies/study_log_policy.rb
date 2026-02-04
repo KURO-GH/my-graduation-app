c7788066fd9da490ea494e25c1183eaf82c0cb65
kuro-gh <kuro@example.com>
2026-02-04 14:06:43 +0900

    Add Pundit authorization for study logs

---
+++ app/policies/study_log_policy.rb
@@ -0,0 +1,41 @@
+class StudyLogPolicy < ApplicationPolicy
+  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
+  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
+  # In most cases the behavior will be identical, but if updating existing
+  # code, beware of possible changes to the ancestors:
+  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  def index?
+    user.present?
+  end
+
+  def show?
+    user.present? && record.user_id == user.id
+  end
+
+  def create?
+    user.present?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.present? && record.user_id == user.id
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    update?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.where(user_id: user.id)
+    end
+  end
+end
