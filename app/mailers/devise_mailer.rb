9e1fe3de7df122ceeff88275b3b741410fd0413c
kuro-gh <kuro@example.com>
2026-02-06 21:00:28 +0900

    Use SendGrid in DeviseMailer for password reset

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,23 +2,32 @@ class DeviseMailer < Devise::Mailer
   default from: 'k.s06020608@gmail.com'
   layout 'mailer'
 
-  # Devise のメソッドをオーバーライド
   def reset_password_instructions(record, token, opts = {})
-    # Devise の元メソッドで URL を作る
-    opts[:subject] = "パスワード再設定"
     @token = token
     @resource = record
+    opts[:subject] ||= "パスワード再設定"
 
-    # SendGrid API で送信
-    mail = SendGrid::Mail.new
-    mail.from = SendGrid::Email.new(email: 'k.s06020608@gmail.com')
-    mail.subject = opts[:subject]
-    mail.to = SendGrid::Email.new(email: record.email)
-    mail.content = SendGrid::Content.new(
+    # SendGrid メール作成
+    from = SendGrid::Email.new(email: 'k.s06020608@gmail.com')
+    to = SendGrid::Email.new(email: record.email)
+    content = SendGrid::Content.new(
       type: 'text/html',
       value: "パスワード再設定リンク: #{edit_password_url(record, reset_password_token: token)}"
     )
+
+    mail = SendGrid::Mail.new
+    mail.from = from
+    mail.subject = opts[:subject]
+    mail.add_content(content)
+
+    # Personalization に宛先をセット
+    personalization = SendGrid::Personalization.new
+    personalization.add_to(to)
+    mail.add_personalization(personalization)
+
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    sg.client.mail._('send').post(request_body: mail.to_json)
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+    Rails.logger.info "SendGrid status: #{response.status_code}"
   end
 end
