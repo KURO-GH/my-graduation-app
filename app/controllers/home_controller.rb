e1e6002aca64b86cd2736dc2fad6d5e1ea18c53d
kuro-gh <kuro@example.com>
2026-02-23 23:01:33 +0900

    使い方ページを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@
 
 class HomeController < ApplicationController
   def index; end
+
+  def how_to_use
+    # ここは今は空でOK。ビュー側で内容を書きます
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# config/routes.rb
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
@@ -5,6 +6,12 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  # トップページ
   root 'home#index'
+
+  # 使い方説明ページ
+  get 'home/how_to_use', to: 'home#how_to_use'
+
+  # 学習記録のCRUD
   resources :study_logs, only: %i[index new create edit update destroy]
 end
