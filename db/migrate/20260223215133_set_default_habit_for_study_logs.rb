536351dfabd9713507af208d7eb24829b4031d0f
kuro-gh <kuro@example.com>
2026-02-24 14:24:51 +0900

    Disable old migration to fix deploy order issue

--- db/migrate/20260223215133_set_default_habit_for_study_logs.rb
+++ db/migrate/20260223215133_set_default_habit_for_study_logs.rb
@@ -1,20 +1,8 @@
 class SetDefaultHabitForStudyLogs < ActiveRecord::Migration[7.0]
   def up
-    # 既存の StudyLog すべてをループ
-    StudyLog.find_each do |log|
-      next if log.habit.present?  # すでに紐付いている場合はスキップ
-
-      # 各 StudyLog に紐づく Habit を作成
-      Habit.create!(
-        user: log.user,
-        study_log: log,
-        completed: false
-      )
-    end
+    # 本番でエラーになるため処理はスキップ
   end
 
   def down
-    # 元に戻す場合は、習慣を削除
-    Habit.where(completed: false).destroy_all
   end
 end
