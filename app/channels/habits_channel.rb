fae6583b7f4e2ea94797b305c39324d92263cefe
kuro-gh <kuro@example.com>
2026-03-04 16:25:51 +0900

    Fix channel path

--- app/channels/habits_channel.rb
+++ app/channels/habits_channel.rb
@@ -1,9 +1,10 @@
+# app/channels/habits_channel.rb
 class HabitsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_for current_user
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    # ここに必要なクリーンアップ処理があれば書く
   end
 end
