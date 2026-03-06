5dc0bada66d1e06dcfc6b0fbc205132cfc066522
kuro-gh <kuro@example.com>
2026-03-06 21:55:19 +0900

    specを一部修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,11 @@
+# spec/requests/static_pages_spec.rb
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /privacy_policy" do
     it "returns http success" do
-      get "/static_pages/privacy_policy"
+      get "/privacy_policy"  # ← ここをルートに合わせて修正
       expect(response).to have_http_status(:success)
     end
   end
-
 end
